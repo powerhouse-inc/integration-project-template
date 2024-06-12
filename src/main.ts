597b3bc2e9d8798bcdfe8716f6f7709a86fef362
Frank <frank.pfeift@protonmail.com>
2024-06-12 11:36:59 +0200

    fix: some issues

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,79 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
+const addFoldersAndDocuments = async (driveServer: DocumentDriveServer, driveName: string) => {
+    let docId = "2000"
+    let drive = await driveServer.getDrive(driveName);
+    let document: ArbLtipGranteeDocument;
+    try {
+        document = (await driveServer.getDocument(driveName, docId)) as ArbLtipGranteeDocument
+    } catch (e) {
+        // add folder
+        drive = reducer(
+            drive,
+            actions.addFolder({
+                id: '2',
+                name: "Folder"
+            })
+        )
+
+        // create new document in folder with generated sync unit
+        drive = reducer(
+            drive,
+            DocumentDriveUtils.generateAddNodeAction(
+                drive.state.global,
+                {
+                    id: docId,
+                    name: 'document 1',
+                    documentType: 'ArbLtipGrantee',
+                },
+                ['global', 'local']
+            )
+        );
+
+        // queue last 2 drive operations
+        await driveServer.addDriveOperations(driveName, drive.operations.global.slice(-2));
+
+        // retrieve new created document
+        document = (await driveServer.getDocument(
+            driveName,
+            docId
+        )) as ArbLtipGranteeDocument;
+    }
+
+
+    document = arbReducer(
+        document,
+        arbActions.initGrantee({
+            authorizedSignerAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C",
+            disbursementContractAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C",
+            fundingAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C",
+            fundingType: ["EOA"],
+            granteeName: "Frank",
+            grantSize: 1_000_000,
+            grantSummary: "arbitrum import script for powerhouse",
+            matchingGrantSize: 1_000_000,
+            metricsDashboardLink: "https://arbgrants.com",
+            startDate: "2024-06-12T12:00:00Z",
+            numberOfPhases: 1,
+            phaseDuration: 1
+        })
+    );
+
+    // create new operations with document model actions
+    // document = arbReducer(
+    //     document,
+    //     arbActions.addEditor({
+    //         editorAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C"
+    //     })
+    // );
+
+    // queue new created operations for processing
+    const result = await driveServer.addOperations(driveName, docId, document.operations.global.slice(-2));
+    console.log(result.document?.state);
+
+}
+
 async function main() {
     // select document models
     const documentModels = [
@@ -34,85 +107,40 @@ async function main() {
 
     const driveName = remoteDriveUrl.split("/")!.slice(-1)[0];
 
+    if (!driveName) {
+        throw new Error("Could not extract drivename from remote Drive URL");
+    }
+
     let drive: DocumentDriveDocument;
-    drive = await driveServer.addRemoteDrive(remoteDriveUrl!, { availableOffline: true, listeners: [], sharingType: "public", triggers: [] });
+    drive = await driveServer.addRemoteDrive(remoteDriveUrl!, {
+        availableOffline: true, listeners: [
+            {
+                block: true,
+                callInfo: {
+                    data: remoteDriveUrl,
+                    name: 'switchboard-push',
+                    transmitterType: 'SwitchboardPush',
+                },
+                filter: {
+                    branch: ['main'],
+                    documentId: ['*'],
+                    documentType: ['*'],
+                    scope: ['global'],
+                },
+                label: 'Switchboard Sync',
+                listenerId: '1',
+                system: true,
+            },
+        ], sharingType: "public", triggers: [], pullInterval: 100
+    });
+
     driveServer.on("syncStatus", async (driveId, syncStatus) => {
         if (driveId !== driveName || syncStatus !== "SUCCESS") {
             return;
         }
 
-        drive = await driveServer.getDrive(driveName);
-        console.log(drive.state.global);
-
-        // add folder
-        drive = reducer(
-            drive,
-            actions.addFolder({
-                id: '2',
-                name: "Folder"
-            })
-        )
-
-        // create new document in folder with generated sync unit
-        drive = reducer(
-            drive,
-            DocumentDriveUtils.generateAddNodeAction(
-                drive.state.global,
-                {
-                    id: '2.1',
-                    name: 'document 1',
-                    documentType: 'ArbLtipGrantee',
-                    parentFolder: "2"
-                },
-                ['global', 'local']
-            )
-        );
-
-        // queue last 2 drive operations
-        const driveResult = await driveServer.addDriveOperations(driveName, drive.operations.global.slice(-2));
-
-        // retrieve new created document
-        let document = (await driveServer.getDocument(
-            driveName,
-            '2.1'
-        )) as ArbLtipGranteeDocument;
-
-        document = arbReducer(
-            document,
-            arbActions.initGrantee({
-                authorizedSignerAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C",
-                disbursementContractAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C",
-                fundingAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C",
-                fundingType: ["EOA"],
-                granteeName: "Frank",
-                grantSize: 1_000_000,
-                grantSummary: "arbitrum import script for powerhouse",
-                matchingGrantSize: 1_000_000,
-                metricsDashboardLink: "https://arbgrants.com",
-                startDate: "2024-06-12T12:00:00Z",
-                numberOfPhases: 1,
-                phaseDuration: 1
-            })
-        );
-
-        // create new operations with document model actions
-        // document = arbReducer(
-        //     document,
-        //     arbActions.addEditor({
-        //         editorAddress: "0x1AD3d72e54Fb0eB46e87F82f77B284FC8a66b16C"
-        //     })
-        // );
-
-
-        try {
-            // queue new created operations for processing
-            const result = await driveServer.addOperations(driveName, '2.1', document.operations.global.slice(-2));
-            console.log(result);
-        } catch (e) {
-            console.error(e);
-        }
+        await addFoldersAndDocuments(driveServer, driveName);
     })
-
 
 }
 
